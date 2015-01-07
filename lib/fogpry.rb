2cbe889b0025dad066cd22e43f4fe0fad5af081d
Atsushi Sasaki <atsaki01@gmail.com>
2015-01-08 02:45:30 +0900

    initial commit

---
+++ lib/fogpry.rb
@@ -0,0 +1,5 @@
+require "fogpry/version"
+require "fogpry/cli"
+
+module Fogpry
+end
---
+++ lib/fogpry/cli.rb
@@ -0,0 +1,37 @@
+require "fogpry"
+require "yaml"
+require "pry"
+require "fog"
+
+module Fogpry
+  class Fogpry
+
+    attr_reader :profiles, :profile, :connection
+
+    def initialize
+      @profiles = YAML.load_file(File.expand_path("~/.fogpry"))
+      @profile = nil
+      @connection = nil
+    end
+
+    def list_profiles
+      @profiles.keys.each do |p|
+        puts p
+      end
+    end
+
+    def connect(profile)
+      @profile = profile
+      @connection = Fog::Compute.new(symbolize(@profiles[@profile]))
+    end
+
+    protected
+
+    def symbolize(hash)
+      hash.keys.each do |key|
+        hash[(key.to_sym rescue key) || key] = hash.delete(key)
+        return hash
+      end
+    end
+  end
+end
